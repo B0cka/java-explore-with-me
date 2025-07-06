39c149c24226a0b5819c4882275e75cf3f4c6a39
B0cka <Bocka1423@yandex.ru>
2025-07-06 15:11:09 +0300

    complete work, debugging_11

--- ewm-service/src/main/java/ru/practicum/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/EwmServiceApplication.java
@@ -1,16 +1,12 @@
 package ru.practicum;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-public class Main {
-
+public class EwmServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(EwmServiceApplication.class, args);
     }
 
 }
---
+++ stats-server/src/main/java/ru/practicum/StatsServerApplication.java
@@ -0,0 +1,16 @@
+package ru.practicum;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class StatsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatsServerApplication.class, args);
+    }
+
+}
