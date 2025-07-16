690360d0d1c2f8d78cbc59b1ae54da3550312a19
B0cka <Bocka1423@yandex.ru>
2025-07-16 15:37:19 +0300

    work on tests

--- ewm-service/src/main/java/ru/practicum/users/controller/UserEventController.java
+++ ewm-service/src/main/java/ru/practicum/users/controller/UserEventController.java
@@ -27,7 +27,7 @@ public class UserEventController {
         return eventService.addNewEvent(userId, input);
     }
 
-    @PatchMapping("/{eventId}")
+    @PatchMapping("/{userId}/events/{eventId}")
     public EventFullDto updateEventByOwner(@PathVariable(value = "userId") @Min(0) Long userId,
                                            @PathVariable(value = "eventId") @Min(0) Long eventId,
                                            @RequestBody @Valid UpdateEventUserRequest inputUpdate, HttpServletRequest request) {
@@ -35,7 +35,7 @@ public class UserEventController {
         return eventService.updateEventByUserIdAndEventId(userId, eventId, inputUpdate);
     }
 
-    @GetMapping
+    @GetMapping("/{userId}/events")
     public List<EventFullDto> getUserEvents(@PathVariable @Min(1) Long userId,
                                             @RequestParam(defaultValue = "0") @Min(0) Integer from,
                                             @RequestParam(defaultValue = "10") @Min(1) Integer size, HttpServletRequest request) {
@@ -43,19 +43,19 @@ public class UserEventController {
         return eventService.getEventsByUserId(userId, from, size);
     }
 
-    @GetMapping("/{eventId}")
+    @GetMapping("/{userId}/events/{eventId}")
     public EventFullDto getUserEventsBuId(@PathVariable @Min(1) Long userId, @PathVariable @Min(1) Long eventId, HttpServletRequest request) {
         statsClient.createHit(request);
         return eventService.getEventById(userId, eventId);
     }
 
-    @GetMapping("/{eventId}/requests")
+    @GetMapping("/{userId}/events/{eventId}/requests")
     public List<ParticipationRequestDto> getRequestsById(@PathVariable(value = "eventId") Long eventId, @PathVariable(value = "userId") Long userId, HttpServletRequest request) {
         statsClient.createHit(request);
         return eventService.getAllParticipationRequestsFromEventByOwner(userId, eventId);
     }
 
-    @PatchMapping("/{eventId}/requests")
+    @PatchMapping("/{userId}/events/{eventId}/requests")
     public EventRequestStatusUpdateResult updateStatusRequestFromOwner(@PathVariable(value = "userId") @Min(1) Long userId,
                                                                        @PathVariable(value = "eventId") @Min(1) Long eventId,
                                                                        @RequestBody EventRequestStatusUpdateRequest inputUpdate) {
