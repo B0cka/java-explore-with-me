22964ee8c3aca1476d2e46ad9955a8c842b2b8e2
B0cka <Bocka1423@yandex.ru>
2025-07-18 11:58:46 +0300

    work on tests

--- ewm-service/src/main/java/ru/practicum/publicApi/PublicEventController.java
+++ ewm-service/src/main/java/ru/practicum/publicApi/PublicEventController.java
@@ -5,10 +5,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.admin.dto.EventFullDto;
 import ru.practicum.admin.dto.EventShortDto;
 import ru.practicum.admin.model.SearchEventParams;
@@ -25,7 +22,7 @@ public class PublicEventController {
     private final EventService eventService;
 
     @GetMapping
-    public List<EventShortDto> getAllEvents(@Valid SearchEventParams searchEventParams,
+    public List<EventShortDto> getAllEvents(@Valid @ModelAttribute SearchEventParams searchEventParams,
                                             HttpServletRequest request) {
         return eventService.getAllEventFromPublic(searchEventParams, request);
     }
